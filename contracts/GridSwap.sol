622857d9536b1c13313d52252651d9c457164a72
shivam920177 <shivamsingh934011@gmail.com>
2025-06-25 13:22:34 +0530

    Update GridSwap.sol

--- contracts/GridSwap.sol
+++ contracts/GridSwap.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GridSwap is Ownable {
-    IERC20 public energyToken;
+    IERC20 public energyToken; 
 
     struct Trade {
         address seller;
